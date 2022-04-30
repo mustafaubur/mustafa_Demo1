af0a9ba91a68064cc1fda8a2bf5dbcb2b559c88f
mustafaubur <uburustafa@gmail.com>
2022-04-30 00:37:32 -0700

    my first commit

--- Test01/src/test.java
+++ Test01/src/test.java
@@ -1,5 +1,9 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int b= 3;
+        boolean f= true;
+        System.out.println( b + 3*b );
+        System.out.println(f);
     }
 }
