9ede85731dd6648a08b9c494aaf28fb593328347
mustafaubur <uburmustafa@gmail.com>
2022-04-30 01:53:01 -0700

    mustafa_Demo1

--- Test01/src/test.java
+++ Test01/src/test.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
@@ -5,5 +7,12 @@ public class test {
         boolean f= true;
         System.out.println( b + 3*b );
         System.out.println(f);
+
+
+        String [] wer={ "hello", "how", "are", "you"};
+        System.out.println(Arrays.toString(wer));
+        System.out.println(Arrays.toString(wer));
+        System.out.println(wer.length-1);
+
     }
 }
